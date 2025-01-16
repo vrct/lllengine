1b486148f94475a52ae56609c491a308474cbc7d
vrct <furkanarican@gmail.com>
2025-01-16 22:05:59 +0100

    performance issue solved on big entity count

---
+++ include/ecs/systems/gravitySystem.h
@@ -0,0 +1,16 @@
+//
+// Created by Furkan ARICAN on 13.01.2025.
+//
+
+#ifndef GRAVITYSYSTEM_H
+#define GRAVITYSYSTEM_H
+
+
+
+class gravitySystem {
+
+};
+
+
+
+#endif //GRAVITYSYSTEM_H
---
+++ include/ecs/systems/system.h
@@ -0,0 +1,8 @@
+//
+// Created by Furkan ARICAN on 15.01.2025.
+//
+
+#ifndef SYSTEM_H
+#define SYSTEM_H
+
+#endif //SYSTEM_H
